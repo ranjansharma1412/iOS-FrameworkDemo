6159d97548aa4405d45300b383a05d0f2b051511
Ranjan Sharma <ranjansharma1412@gmail.com>
2025-09-13 11:27:04 +0530

    Initial Commit

---
+++ FrameworksDemo/FrameworksDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksDemoApp.swift
+//  FrameworksDemo
+//
+//  Created by Ranjan sharma on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrameworksDemoTests/FrameworksDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksDemoTests.swift
+//  FrameworksDemoTests
+//
+//  Created by Ranjan sharma on 13/09/25.
+//
+
+import Testing
+@testable import FrameworksDemo
+
+struct FrameworksDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
